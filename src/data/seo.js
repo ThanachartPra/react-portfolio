6b85e975c7fb8ba6469ca40a011597e7a93b1934
ThanachartPra <110073990+ThanachartPra@users.noreply.github.com>
2023-11-12 20:46:04 +0700

    first commit

---
+++ src/data/seo.js
@@ -0,0 +1,31 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"My name is Thanachart Prasertsuk, you can call me Moss.Now I am studying at King Mongkut's University of Technology North Bangkok, Computer Electronics Department. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+		keywords: ["Thanachart", "P Thanachart", "Thanachart Prasertsuk"],
+	},
+
+	{
+		page: "about",
+		description:
+			"You can call me Moss.Now I am studying at King Mongkut's University of Technology North Bangkok, Computer Electronics Department. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+		keywords: ["Thanachart", "P Thanachart", "Thanachart Prasertsuk"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+		keywords: ["Thanachart", "P Thanachart", "Thanachart Prasertsuk"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"",
+		keywords: ["Thanachart", "P Thanachart", "Thanachart Prasertsuk"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,44 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Thanachart",
+		name: "P.Thanachart",
+		email: "mossloveml5@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/ThanachartPra",
+		facebook: "https://www.facebook.com/moss.pra/",
+	},
+
+	homepage: {
+		title: "Welcome to my portfolio",
+		description:
+			"My name is Thanachart Prasertsuk, you can call me Moss.Now I am studying at King Mongkut's University of Technology North Bangkok, Computer Electronics Department. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer."
+	},
+
+	about: {
+		title: "I’m Thanachart Prasertsuk",
+		description:
+			"You can call me Moss.Now I am studying at King Mongkut's University of Technology North Bangkok, Computer Electronics Department. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	/*articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},*/
+
+	projects: [
+		{
+			title: "Project Food Webboard",
+			description:
+				"First group porject",
+			logo: "https://www.php.net//images/logos/new-php-logo.svg",
+			linkText: "View Project",
+			link: "https://github.com/prwthub/project_foodwebboard",
+		},
+	],
+};
+
+export default INFO;
